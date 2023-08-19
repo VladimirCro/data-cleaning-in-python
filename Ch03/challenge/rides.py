7f5b1c7f6d07217329820e7a2218de692f1edd46
Vladimir <130163252+VladimirCro@users.noreply.github.com>
2023-08-19 09:42:12 +0000

    My work

--- Ch03/challenge/rides.py
+++ Ch03/challenge/rides.py
@@ -5,6 +5,23 @@ df = pd.read_csv('rides.csv')
 df
 # %%
 # Find out all the rows that have bad values
+df[df.isnull().any(axis=1)]
+# %%
 # - Missing values are not allowed
-# - A plate must be a combination of at least 3 upper case letters or digits
+import numpy as np
+mask = df['plate'].str.strip() == ''
+df.loc[mask, 'plate'] = np.nan
+df.dropna(inplace=True)
 # - Distance much be bigger than 0
+df = df[df['distance'] > 0]
+# %%
+df
+# %%
+# - A plate must be a combination of at least 3 upper case letters or digits
+def has_min_upper_or_digits(plate, min_count):
+    upper_or_digit_count = sum(1 for char in plate if char.isupper() or char.isdigit())
+    return upper_or_digit_count >= min_count
+min_count = 3
+filtered_df = df[df['plate'].apply(lambda x: has_min_upper_or_digits(x, min_count))]
+filtered_df
+# %%
